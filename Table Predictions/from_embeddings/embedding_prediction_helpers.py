1e125d9fa0e3adbfb69a7623d577181592f56f8d
egekurt123 <egekurt14@gmail.com>
2025-05-26 23:31:38 +0200

    Add DMF score predictions

--- Table Predictions/from_embeddings/embedding_prediction_helpers.py
+++ Table Predictions/from_embeddings/embedding_prediction_helpers.py
@@ -9,10 +9,16 @@ import matplotlib.pyplot as plt
 import random
 import xgboost as xgb
 
-interaction_table = pd.read_csv('../../extracted_data/interaction_table_all.csv', sep=',', index_col=0)
-interaction_table
 
-def preprocess_data(embeddings, orthologous_groups = False):
+def preprocess_data(embeddings, orthologous_groups = False, prediction_type = 'gi_score', subfolder=True):
+
+    path = '../../extracted_data/interaction_table_all.csv'
+
+    if subfolder:
+        path = '../' + path
+
+    interaction_table = pd.read_csv(path, sep=',', index_col=0)
+
     available_genes = set(embeddings.index)
     filtered = interaction_table[
         interaction_table['query_gene'].isin(available_genes) &
@@ -27,7 +33,7 @@ def preprocess_data(embeddings, orthologous_groups = False):
         for _, row in filtered_sample.iterrows()
     ])
 
-    y = filtered_sample['gi_score'].values
+    y = filtered_sample[prediction_type].values
     
     return X, y
 
@@ -55,17 +61,18 @@ def run_Linear_Regression(X, y, color, plot=True, pca=False):
         plt.show()
 
 
-def run_PCA(X):
+def run_PCA(X, plot=True):
     N=128
     pca = PCA(n_components=N)
     X_PCA = pca.fit_transform(X.copy())
     ev = pca.explained_variance_ratio_.cumsum()
-    print(ev[-1])
-    plt.plot(range(0, N), ev)
+    if plot:
+        print(ev[-1])
+        plt.plot(range(0, N), ev)
     return X_PCA
 
 
-def run_XGBoost(X, y, plot=True):
+def run_XGBoost(X, y, embeddings=None, plot=True):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=43)
 
     xgb_model = xgb.XGBRegressor(
@@ -82,13 +89,21 @@ def run_XGBoost(X, y, plot=True):
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
     print("XGBoost Regression")
-    print(f"XGBoost R² score: {r2:.3f}")
-    print(f"XGBoost RMSE: {rmse:.3f}")
+    print(f"R2: {r2:.3f}")
+    print(f"RMSE: {rmse:.3f}")
 
     if plot:
+        emb_cols = embeddings.columns.tolist()
+        feature_names = [f"query_{col}" for col in emb_cols] + [f"array_{col}" for col in emb_cols]
+        
+        importances = xgb_model.feature_importances_
+        indices = np.argsort(importances)[-20:]
+        
         plt.figure(figsize=(10, 6))
-        xgb.plot_importance(xgb_model, max_num_features=20)
+        plt.barh(range(len(indices)), importances[indices])
+        plt.yticks(range(len(indices)), [feature_names[i] for i in indices])
         plt.title('XGBoost Feature Importance')
+        plt.tight_layout()
         plt.show()
 
 
@@ -113,35 +128,43 @@ def run_Random_Forest(X, y, embeddings, plot=True):
     print(f"Random Forest R² score: {r2:.3f}")
     print(f"Random Forest RMSE: {rmse:.3f}")
 
-    # Add the column name
-    emb_cols = embeddings.columns.tolist()
-    n_emb_dims = len(emb_cols)
-    feature_names = [f"query_{col}" for col in emb_cols] + [f"array_{col}" for col in emb_cols]
-    #######################
-
-    plt.figure(figsize=(10, 6))
-    importances = rf_model.feature_importances_
-    indices = np.argsort(importances)[-20:] 
 
     if plot:
+        # Add the column name
+        emb_cols = embeddings.columns.tolist()
+        n_emb_dims = len(emb_cols)
+        feature_names = [f"query_{col}" for col in emb_cols] + [f"array_{col}" for col in emb_cols]
+        #######################
+
+        plt.figure(figsize=(10, 6))
+        importances = rf_model.feature_importances_
+        indices = np.argsort(importances)[-20:] 
+
         plt.barh(range(len(indices)), importances[indices])
         plt.yticks(range(len(indices)), [feature_names[i] for i in indices])
         plt.title('Random Forest Feature Importance')
         plt.tight_layout()
         plt.show()
 
-def predict_all_models(embeddings, combination):
+def predict_all_models(embeddings, combination, prediction_type='gi_score'):
 
     print("Running for combination:", combination)
+    print("\n")
 
-    X,y = preprocess_data(embeddings)
+    X,y = preprocess_data(embeddings, prediction_type=prediction_type, subfolder=False)
 
     run_Linear_Regression(X, y, "darkblue", plot=False, pca=False)
+    print("\n")
 
-    X_PCA = run_PCA(X)
+    X_PCA = run_PCA(X, plot=False)
 
     run_Linear_Regression(X_PCA, y, "darkred", plot=False, pca=True)
+    print("\n")
+
 
     run_XGBoost(X, y, plot=False)
+    print("\n")
 
     run_Random_Forest(X, y, embeddings, plot=False)
+    print("\n")
+
